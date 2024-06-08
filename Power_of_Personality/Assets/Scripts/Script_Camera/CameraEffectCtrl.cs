a5e28f58987ae552fbdb3624cf9576eee05a242f
DESKTOP-0NKI1DS\ㅂㄱㅇ <qorrjsdn668@daum.net>
2024-06-08 13:54:31 +0900

    2024-06-08 카메라 이펙트/카메라 스크립트 변경 중

--- Power_of_Personality/Assets/Scripts/Script_Camera/CameraEffectCtrl.cs
+++ Power_of_Personality/Assets/Scripts/Script_Camera/CameraEffectCtrl.cs
@@ -69,48 +69,86 @@ public class CameraEffectCtrl : MonoBehaviour
         {
             ResetCameraEffect();
         }
+
+        BlurCamera_Set();
+        ColorDiffuse_Set();
+
     }
 
     #region 값 범용적 효과들
 
-    public void BlurCamera(float BlurValue)
+    public IEnumerator BlurCamera(float BlurValue)
     {
         // Depth Of Field의 블러 강도 조정
         depthOfField.active = true; // Depth Of Field 효과 활성화
         depthOfField.focusDistance.overrideState = true; // 효과 내 Focus Distance 항목 활성화
-        depthOfField.focusDistance.value = BlurValue; // 포커스 거리 설정(0 ~ 100)
+        float blurTime = 0;
+        yield return new WaitForSeconds(0.1f);
+        while(blurTime == 1.5f)
+        {
+            blurTime += Time.deltaTime;
+            depthOfField.focusDistance.value = Mathf.Lerp(3, BlurValue, Time.deltaTime); // 포커스 거리 설정(0 ~ 100)
+        }
     }
 
-    public void ColorDiffuse(float DiffuseValue)
+    public void BlurCamera_Set()
+    {
+
+    }
+
+    public IEnumerator ColorDiffuse(float DiffuseValue)
     {
         chromaticAberration.active = true; // Chromatic Aberration 효과 활성화
         chromaticAberration.intensity.overrideState = true; // 효과 내 Intensity 항목 활성화
-        chromaticAberration.intensity.value = DiffuseValue; // 색번짐 범위 설정(0 ~ 1)
+        float diffuseTime = 0;
+        yield return new WaitForSeconds(0.1f);
+        while(diffuseTime == 1.5f)
+        {
+            diffuseTime  += Time.deltaTime;
+            chromaticAberration.intensity.value  = Mathf.Lerp(0, DiffuseValue, Time.deltaTime); // 포커스 거리 설정(0 ~ 100)
+        }
     }
 
-    public void ColorFilter(float FilterR, float FilterG, float FilterB, float contrastValue)
+    public void ColorDiffuse_Set()
+    {
+        
+    }
+
+    public IEnumerator ColorFilter(float FilterR, float FilterG, float FilterB, float contrastValue)
     {
         colorAdjustments.active = true; // 효과 활성화
         colorAdjustments.contrast.overrideState = true; //효과 내 contrast 항목 활성화
         colorAdjustments.colorFilter.overrideState = true;
-        colorAdjustments.contrast.value = contrastValue;
+        /*
+        startValue = 0;
+        endValue = contrastValue;
+        timeSpeed = 1;
+        */
+        yield return new WaitForSeconds(0.1f);
+        colorAdjustments.contrast.value = Mathf.Lerp(0, contrastValue, Time.deltaTime);
         colorAdjustments.colorFilter.value = new Color(FilterR, FilterG, FilterB, 1f);
     }
 
-    public void WeakZoom(int zoomValue)
+    public IEnumerator WeakZoom(int zoomValue)
     {
         paniniProjection.active = true;
         paniniProjection.distance.overrideState = true;
+        yield return new WaitForSeconds(0.1f);
         paniniProjection.distance.value = zoomValue;
     }
-
-    public void RoundCamera(float roundValue, float colorR, float colorG, float colorB)
+    
+    public IEnumerator RoundCamera(float roundValue, float colorR, float colorG, float colorB)
     {
         vignette.active = true;
         vignette.color.overrideState = true;
         vignette.color.value = new Color(colorR, colorG, colorB, 1f);
-        vignette.intensity.value = roundValue;
-    }
+        yield return new WaitForSeconds(0.1f);
+        vignette.intensity.value = Mathf.Lerp(0, roundValue, 1f); // 포커스 거리 설정(0 ~ 100)
+        yield return new WaitForSeconds(1.1f);
+        vignette.intensity.value = Mathf.Lerp(roundValue, 0, 1f); // 포커스 거리 설정(0 ~ 100)
+        yield return new WaitForSeconds(0.1f);
+        //vignette.active = false;
+    }   
 
     #endregion
 
@@ -118,9 +156,9 @@ public class CameraEffectCtrl : MonoBehaviour
 
     public void DamageCamera()
     {
-        BlurCamera(2f);
-        ColorDiffuse(0.4f);
-        RoundCamera(0.4f, 1, 0, 0);
+        StartCoroutine(BlurCamera(2f));
+        StartCoroutine(ColorDiffuse(0.4f));
+        StartCoroutine(RoundCamera(0.4f, 1, 0, 0));
     }
 
     #endregion
@@ -130,11 +168,13 @@ public class CameraEffectCtrl : MonoBehaviour
     public void ResetCameraEffect()
     {
         //서서히 바뀌는거 추가할 예정
+        /*
         depthOfField.active = false; 
         chromaticAberration.active = false; 
         colorAdjustments.active = false; 
         paniniProjection.active = false;
         vignette.active = false;
+        */
     }
 
     #endregion
