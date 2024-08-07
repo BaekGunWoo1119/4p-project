a79fae25f96938f6f27ab48856376dd22c003b3c
Jungdh1669 <jdh1669@naver.com>
2024-08-07 13:37:38 +0900

    2024-08-07 정도훈 : 서버 중간 업로드

--- Power_of_Personality/Assets/Server/MultiGameManager.cs
+++ Power_of_Personality/Assets/Server/MultiGameManager.cs
@@ -19,12 +19,13 @@ public class MultiGameManager : MonoBehaviourPunCallbacks
     public int min; // 타이머용 시간
     public Transform[] SpawnPoints; // 몬스터 스폰 포인트들
     public Transform[] StageSpawnPoints; // 스테이지별 스폰 포인트들
-    public Transform ShopTr; //상점 위치
+    public Transform ShopTr; // 상점 위치
     public TMP_Text timerText;
     public GameObject Player;
-    public bool IsDie; //죽었는지 판단
+    public bool IsDie; // 죽었는지 판단
     private WaveDatas JSONWaveList; // JSON에서 받아온 웨이브 데이터
-    public bool IsWave; //현재 웨이브 진행중인지
+    public bool IsWave; // 현재 웨이브 진행 중인지
+    public Collider PlayerCol; // 플레이어 콜라이더
 
     #region JSON 관련 스크립트
     // JSON 데이터를 저장할 클래스 정의
@@ -51,7 +52,6 @@ public class MultiGameManager : MonoBehaviourPunCallbacks
             Spawned = 0; // 초기화 추가
         }
     }
-
     #endregion
 
     // Start is called before the first frame update
@@ -62,6 +62,7 @@ public class MultiGameManager : MonoBehaviourPunCallbacks
         sec = 0f;
         min = 0;
         Spawned = 0; // 초기화 추가
+        IsDie = false;
 
         string jsondata = Resources.Load<TextAsset>("JSON/WaveData").text;
         // JSON 데이터를 WaveDatas 클래스로 Deserialize
@@ -71,9 +72,15 @@ public class MultiGameManager : MonoBehaviourPunCallbacks
 
     void Update()
     {
-        if (!Player == null){
-            Player = GameObject.FindWithTag("Player"); //플레이어 오브젝트 찾기
+        if (Player == null)
+        {
+            Player = GameObject.FindWithTag("Player"); // 플레이어 오브젝트 찾기
+            if (Player != null)
+            {
+                PlayerCol = Player.GetComponent<Collider>();
+            }
         }
+
         #region 인게임 타이머
         sec += Time.deltaTime;
         if (sec >= 60f)
@@ -82,7 +89,6 @@ public class MultiGameManager : MonoBehaviourPunCallbacks
             sec = 0;
         }
 
-        // 인게임에 UI 넣어야 됨 고민해야 될 내용(수정완료)
         timerText.text = string.Format("{0:D2}:{1:D2}", min, (int)sec);
         #endregion
 
@@ -94,7 +100,7 @@ public class MultiGameManager : MonoBehaviourPunCallbacks
         if (CurrentTime > WaveTime)
         {
             // 몬스터가 0명이라면
-            if (GameObject.FindGameObjectsWithTag("Monster").Length < 1)
+            if (MonsterCount < 1)
             {
                 EndWave();
                 WaveUpdate();
@@ -108,17 +114,33 @@ public class MultiGameManager : MonoBehaviourPunCallbacks
                 SpawnMonster();
             }
         }
-        //웨이브 끝났을 때 상점나왔는지 체크
-        if( IsWave == false){
-            if(CheckExitShop() == true){
-                //startwave();
+
+        // 웨이브 끝났을 때 상점 나왔는지 체크
+        if (IsWave == false)
+        {
+            ExitShop();
+            if (CheckExitShop() == true)
+            {
+                // startwave();
             }
+        }
+        //죽으면 다른 플레이어 관전(UI추가해야함)
+        if (Status.HP<=0){
+            IsDie = true;
+            CameraCtrl.target = GameObject.FindGameObjectWithTag("OtherPlayer").transform;
+        }
+        else {
+            CameraCtrl.target = GameObject.FindGameObjectWithTag("Player").transform;
         }
     }
 
     // 웨이브 종료 후 상점으로 이동
     void EndWave()
     {
+        if (IsDie == true){
+            IsDie = false;
+            MultiPlayStart.SpawnPlayer();
+        }
         IsWave = false;
         // 체력 회복
         Status.HP = Status.MaxHP;
@@ -126,14 +148,14 @@ public class MultiGameManager : MonoBehaviourPunCallbacks
         CurrentTime = 0f; // 초기화 추가
         WaveUpdate(); // 다음 웨이브 설정
         // 상점으로 이동 (상점 스크립트 바꿔서 적용해야 할 듯)
-        // 상점에서 나가면은 레디가 되도록 스크립트 새로 만들던가
-        Player.transform.position =ShopTr.position;
+        GameObject.Find("EventSystem").GetComponent<Shop_PortalCtrl>().Open_Shop(PlayerCol);
+        Player.transform.position = ShopTr.position;
     }
 
     // 몬스터 스폰
     void SpawnMonster()
-    {   
-        //호스트에서 몹 소환하도록
+    {
+        // 호스트에서 몹 소환하도록
         if (PhotonNetwork.IsMasterClient)
         {
             int spawnIndex = Random.Range(0, SpawnPoints.Length);
@@ -148,65 +170,61 @@ public class MultiGameManager : MonoBehaviourPunCallbacks
         TempTime = 0f; // 리스폰 대기시간 초기화
     }
 
-    //스폰포인트로 플레이어 이동 후 웨이브 시작
-    void StartWave(){
-        LobbyManager.PlayerProperties["IsExitShop"]=false;
+    // 스폰포인트로 플레이어 이동 후 웨이브 시작
+    void StartWave()
+    {
+        var properties = PhotonNetwork.LocalPlayer.CustomProperties;
+        properties["IsExitShop"] = false;
+        PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
+
         IsWave = true;
-        switch (CurrentWave){
-            case 1: 
-            Player.transform.position = StageSpawnPoints[0].position;
-            break;
-            case 2: 
-            Player.transform.position = StageSpawnPoints[0].position;
-            break;
-            case 3: 
-            Player.transform.position = StageSpawnPoints[0].position;
-            break;
-            case 4: 
-            Player.transform.position = StageSpawnPoints[1].position;
-            break;
-            case 5: 
-            Player.transform.position = StageSpawnPoints[1].position;
-            break;
-            case 6: 
-            Player.transform.position = StageSpawnPoints[1].position;
-            break;
-            case 7: 
-            Player.transform.position = StageSpawnPoints[2].position;
-            break;
-            case 8: 
-            Player.transform.position = StageSpawnPoints[2].position;
-            break;
-            case 9: 
-            Player.transform.position = StageSpawnPoints[2].position;
-            break;
-            case 10: 
-            Player.transform.position = StageSpawnPoints[3].position;
-            break;
-            case 11: 
-            Player.transform.position = StageSpawnPoints[3].position;
-            break;
-            case 12: 
-            Player.transform.position = StageSpawnPoints[3].position;
-            break;
-            
+        switch (CurrentWave)
+        {
+            case 1:
+            case 2:
+            case 3:
+                Player.transform.position = StageSpawnPoints[0].position;
+                break;
+            case 4:
+            case 5:
+            case 6:
+                Player.transform.position = StageSpawnPoints[1].position;
+                break;
+            case 7:
+            case 8:
+            case 9:
+                Player.transform.position = StageSpawnPoints[2].position;
+                break;
+            case 10:
+            case 11:
+            case 12:
+                Player.transform.position = StageSpawnPoints[3].position;
+                break;
         }
     }
 
-    void ExitShop(){
-        //플레이어가 상점 나오면 서버로 응답 보냄
-        if (PlayerCtrl.isShop == false){
-            LobbyManager.PlayerProperties["IsExitShop"]=true;
+    public void ExitShop()
+    {
+        // 플레이어가 상점에서 나오면 서버로 응답 보냄
+        if (PlayerCtrl.isShop == false)
+        {
+            var properties = PhotonNetwork.LocalPlayer.CustomProperties;
+            properties["IsExitShop"] = true;
+            PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
         }
     }
 
-    public bool CheckExitShop(){
+    public bool CheckExitShop()
+    {
         bool IsAllReady = true;
-        //플레이어 모두가 레디라면은
-        foreach(Player Player in PhotonNetwork.PlayerList){
-            if((bool)Player.CustomProperties["IsExitShop"] == false){
+        // 모든 플레이어가 레디 상태인지 확인
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            if ((bool)player.CustomProperties["IsExitShop"] == false)
+            {
                 IsAllReady = false;
-            };
+                break;
+            }
         }
         return IsAllReady;
     }
--- Power_of_Personality/Assets/Server/MultiPlayStart.cs
+++ Power_of_Personality/Assets/Server/MultiPlayStart.cs
@@ -9,49 +9,60 @@ public class MultiPlayStart : MonoBehaviourPunCallbacks
     public Transform SpawnPoint1;
     public Transform SpawnPoint2;
     public Transform SpawnPoint3;
-    private GameObject Player_Character;
+    private static GameObject Player_Character;
     // private GameObject Monster1;
     // private GameObject Monster2;
     // private GameObject Monster3;
-    private string PlayerClass;
+    public static string PlayerClass;
     // Start is called before the first frame update
     void Start()
     {
-        PlayerClass = PlayerPrefs.GetString("PlayerClass");
-        // Transform SpawnPoint1 = GameObject.Find("SpawnPoint1").GetComponent<Transform>();
-        // Transform SpawnPoint2 = GameObject.Find("SpawnPoint2").GetComponent<Transform>();
-        // Transform SpawnPoint3 = GameObject.Find("SpawnPoint3").GetComponent<Transform>();
-        if(PlayerClass == "Wizard"){
-            Player_Character = PhotonNetwork.Instantiate("Server/Wizard/Server_Player_wizard",SpawnPoint1.position,SpawnPoint1.rotation,0);
-        }
-        if(PlayerClass == "Archer"){
-            Player_Character = PhotonNetwork.Instantiate("Server/Archer/Server_Player_archer",SpawnPoint1.position,SpawnPoint1.rotation,0);
-        }
-        if(PlayerClass == "Warrior"){
-            Player_Character = PhotonNetwork.Instantiate("Server/Warrior/Server_Player_warrior",SpawnPoint1.position,SpawnPoint1.rotation,0);
-        }
-        if(PlayerClass == "Rogue"){
-            Player_Character = PhotonNetwork.Instantiate("Server/Rogue/Server_Player_rogue",SpawnPoint1.position,SpawnPoint1.rotation,0);
-        }
-        Player_Character.transform.SetParent(this.transform);
-        
+        SpawnPlayer();
     }
  
     // Update is called once per frame
     void Update()
     {
         Debug.Log(GameObject.FindGameObjectsWithTag("Monster"));
-        if(GameObject.FindGameObjectsWithTag("Monster").Length<1){
-            SpawnMonster();
-
+        if(GameObject.FindGameObjectsWithTag("Monster").Length < 1)
+        {
+            //SpawnMonster();
         }
     }
 
-    void SpawnMonster(){
-        if (PhotonNetwork.IsMasterClient){
-            PhotonNetwork.Instantiate("Server/Monster/Prefab_Cave/Server_Spider",SpawnPoint1.position,SpawnPoint1.rotation,0);
-            PhotonNetwork.Instantiate("Server/Monster/Prefab_Cave/Server_Mimic",SpawnPoint2.position,SpawnPoint2.rotation,0);
-            PhotonNetwork.Instantiate("Server/Monster/Prefab_Cave/Server_Golem",SpawnPoint3.position,SpawnPoint3.rotation,0);
+    void SpawnMonster()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate("Server/Monster/Prefab_Cave/Server_Spider", SpawnPoint1.position, SpawnPoint1.rotation, 0);
+            PhotonNetwork.Instantiate("Server/Monster/Prefab_Cave/Server_Mimic", SpawnPoint2.position, SpawnPoint2.rotation, 0);
+            PhotonNetwork.Instantiate("Server/Monster/Prefab_Cave/Server_Golem", SpawnPoint3.position, SpawnPoint3.rotation, 0);
+        }
+    }
+
+    public static void SpawnPlayer()
+    {
+        PlayerClass = PlayerPrefs.GetString("PlayerClass");
+        MultiPlayStart instance = FindObjectOfType<MultiPlayStart>(); // Find an instance of the class to access instance members
+        if (instance != null)
+        {
+            if (PlayerClass == "Wizard")
+            {
+                Player_Character = PhotonNetwork.Instantiate("Server/Wizard/Server_Player_wizard", instance.SpawnPoint1.position, instance.SpawnPoint1.rotation, 0);
+            }
+            else if (PlayerClass == "Archer")
+            {
+                Player_Character = PhotonNetwork.Instantiate("Server/Archer/Server_Player_archer", instance.SpawnPoint1.position, instance.SpawnPoint1.rotation, 0);
+            }
+            else if (PlayerClass == "Warrior")
+            {
+                Player_Character = PhotonNetwork.Instantiate("Server/Warrior/Server_Player_warrior", instance.SpawnPoint1.position, instance.SpawnPoint1.rotation, 0);
+            }
+            else if (PlayerClass == "Rogue")
+            {
+                Player_Character = PhotonNetwork.Instantiate("Server/Rogue/Server_Player_rogue", instance.SpawnPoint1.position, instance.SpawnPoint1.rotation, 0);
+            }
+            Player_Character.transform.SetParent(instance.transform);
         }
     }
 }
