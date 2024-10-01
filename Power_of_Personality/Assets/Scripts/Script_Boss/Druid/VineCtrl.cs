952f9f632dc03d9636730435b3856d6e974c740b
DESKTOP-IMUE91T\KJT <wjdxo2009@gmail.com>
2024-10-01 17:16:16 +0900

    드루이드 공격 콜라이더 변경

--- Power_of_Personality/Assets/Scripts/Script_Boss/Druid/VineCtrl.cs
+++ Power_of_Personality/Assets/Scripts/Script_Boss/Druid/VineCtrl.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class VineCtrl : MonoBehaviour
 {
+    public GameObject Vine_Collider;
+    public float Vine_xGrowthRate = 10;
+    public float maxDistance;
+
     public float ATK = 20;
+
+    void Update()
+    {
+        if (Vine_Collider.transform.localScale.x <= maxDistance)
+        {
+            Vector3 newScale = Vine_Collider.transform.localScale;
+            newScale.x += Vine_xGrowthRate * Time.deltaTime;
+            Vine_Collider.transform.localScale = newScale;
+        }    
+    }
 }
--- Power_of_Personality/Assets/Scripts/Script_Boss/Ogre/Orge_StraightTentacleCtrl.cs
+++ Power_of_Personality/Assets/Scripts/Script_Boss/Ogre/Orge_StraightTentacleCtrl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Orge_StraightTentacleCtrl : MonoBehaviour
 {
     public GameObject Tentacle_Collider;
-    public float Tentacle_xGrowthRate = 10;
+    public float Tentacle_zGrowthRate = 10;
     public float maxDistance;
 
     public float ATK = 30;
@@ -15,7 +15,7 @@ public class Orge_StraightTentacleCtrl : MonoBehaviour
         if (Tentacle_Collider.transform.localScale.z <= maxDistance)
         {
             Vector3 newScale = Tentacle_Collider.transform.localScale;
-            newScale.z += Tentacle_xGrowthRate * Time.deltaTime;
+            newScale.z += Tentacle_zGrowthRate * Time.deltaTime;
             Tentacle_Collider.transform.localScale = newScale;
         }    
     }
